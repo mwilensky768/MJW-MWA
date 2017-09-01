420f05b0584baa938f519d5de62e18b55589fd42
Mike Wilensky <mjw768@uw.edu>
2017-09-01 14:47:56 -0700

    Pre-Diffuse, Ready for testing

--- Catalog_Run.py
+++ Catalog_Run.py
@@ -2,23 +2,22 @@ import rfipy as rfi
 import argparse
 import os
 
-with open('/nfs/eor-00/h1/mwilensk/FHD/obs_list/beardsley_thesis_list.txt') as f:
+with open('/nfs/eor-00/h1/mwilensk/RFI_Diagnostic_Diffuse_2015/sidelobe_survey_obsIDs.txt') as f:
     obslist = f.read().split("\n")
-with open('/nfs/eor-00/h1/mwilensk/FHD/obs_list/beardsley_thesis_list_paths.txt') as g:
+with open('/nfs/eor-00/h1/mwilensk/RFI_Diagnostic_Diffuse_2015/sidelove_survey_obsIDs_paths.txt') as g:
     pathlist = g.read().split("\n")
 
 parser = argparse.ArgumentParser()
 parser.add_argument("id", type=int)
 args = parser.parse_args()
 
-if not os.path.exists('/nfs/eor-00/h1/mwilensk/long_run_hist_8s/'
-                        + str(obslist[args.id - 1]) + '_hist.npy'):
+if not os.path.exists('/nfs/eor-00/h1/mwilensk/RFI_Diagnostic_Diffuse_2015/' +
+                      str(obslist[args.id - 1]) + 'RFI_Diagnostic_All.png'):
 
     RFI = rfi.RFI()
 
     RFI.rfi_catalog([obslist[args.id - 1], ], pathlist[args.id - 1],
-                    '/nfs/eor-00/h1/mwilensk/RFI_Diagnostic_Long_Run_8s/',
-                    bad_time_indices=[0, 53, 54, 55], hist_write=True,
-                    hist_write_path='/nfs/eor-00/h1/mwilensk/long_run_hist_8s/')
+                    '/nfs/eor-00/h1/mwilensk/RFI_Diagnostic_Diffuse_2015/', hist_write=True,
+                    hist_write_path='/nfs/eor-00/h1/mwilensk/RFI_Diagnostic_Diffuse_2015/Diffuse_2015_Hists/')
 else:
     print('I already processed obs ' + str(obslist[args.id - 1]))
--- write_paths_list.sh
+++ write_paths_list.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-obs_file_name='/nfs/eor-00/h1/mwilensk/FHD/obs_list/beardsley_thesis_list.txt'
-obs_path_file_name='/nfs/eor-00/h1/mwilensk/FHD/obs_list/beardsley_thesis_list_paths.txt'
+obs_file_name='/nfs/eor-00/h1/mwilensk/RFI_Diagnostic_Diffuse_2015/sidelobe_survey_obsIDs.txt'
+obs_path_file_name='/nfs/eor-00/h1/mwilensk/RFI_Diagnostic_Diffuse_2015/sidelove_survey_obsIDs_paths.txt'
 
-for i in {1..1029}
+for i in {1..934}
 do
 	obs=$(sed -n "${i}p" ${obs_file_name})
 	obs_path="$(python /nfs/eor-00/h1/mwilensk/MWA_Tools/scripts/read_uvfits_loc.py -v 4 -s 1 -o ${obs})"
