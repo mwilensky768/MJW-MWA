f6807f767a5b2da052765b29a3afa8f799213d38
mwilensky768 <mjw768@uw.edu>
2017-09-05 19:58:40 -0400

    About to drill

--- write_paths_list.sh
+++ write_paths_list.sh
@@ -6,6 +6,6 @@ obs_path_file_name='/nfs/eor-00/h1/mwilensk/RFI_Diagnostic_Diffuse_2015/sidelobe
 for i in {1..934}
 do
 	obs=$(sed -n "${i}p" ${obs_file_name})
-	obs_path="$(python /nfs/eor-00/h1/mwilensk/MWA_Tools/scripts/read_uvfits_loc.py -v 4 -s 1 -o ${obs})"
+	obs_path="$(python /nfs/eor-00/h1/mwilensk/MWA_Tools/scripts/read_uvfits_loc.py -v 5 -s 1 -o ${obs})"
 	echo "${obs_path}" >> $obs_path_file_name
 done
