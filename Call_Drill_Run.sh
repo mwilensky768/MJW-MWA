f03f95d3d36dae841415c1f5bf28051d7a18ed5e
mwilensky768 <mjw768@uw.edu>
2017-09-11 18:21:24 -0400

    Post Golden Set Drill

---
+++ Call_Drill_Run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#$ -V
+#$ -N Broadband_Drill
+#$ -S /bin/bash
+
+echo JOBID ${JOB_ID}
+echo TASKID ${SGE_TASK_ID}
+
+python /nfs/eor-00/h1/mwilensk/MJW-MWA/Drill_Run.py ${SGE_TASK_ID}
--- write_paths_list.sh
+++ write_paths_list.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-obs_file_name='/nfs/eor-00/h1/mwilensk/RFI_Diagnostic_Diffuse_2015/sidelobe_survey_obsIDs.txt'
-obs_path_file_name='/nfs/eor-00/h1/mwilensk/RFI_Diagnostic_Diffuse_2015/sidelobe_survey_obsIDs_paths.txt'
+obs_file_name='/nfs/eor-00/h1/mwilensk/RunTexts/Broadband_OBSIDS.txt'
+obs_path_file_name='/nfs/eor-00/h1/mwilensk/RunTexts/Broadband_OBSIDS_paths.txt'
 
-for i in {1..934}
+for i in {1..17}
 do
 	obs=$(sed -n "${i}p" ${obs_file_name})
-	obs_path="$(python /nfs/eor-00/h1/mwilensk/MWA_Tools/scripts/read_uvfits_loc.py -v 5 -s 1 -o ${obs})"
+	obs_path="$(python /nfs/eor-00/h1/mwilensk/MWA_Tools/scripts/read_uvfits_loc.py -v 4 -s 1 -o ${obs})"
 	echo "${obs_path}" >> $obs_path_file_name
 done
