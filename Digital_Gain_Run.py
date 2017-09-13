76ee122f704b038ddf376452d0db38886c7dd2b8
Mike Wilensky <mjw768@uw.edu>
2017-09-12 23:24:18 -0700

    Fixed outpath bug on DGC_Run

--- Digital_Gain_Run.py
+++ Digital_Gain_Run.py
@@ -17,6 +17,6 @@ if not os.path.exists('/nfs/eor-00/h1/mwilensk/RFI_Diagnostic_Golden_Set/Digital
     RFI = rfi.RFI()
 
     RFI.digital_gain_compare(obslist[args.id - 1], pathlist[args.id - 1],
-                             '/nfs/eor-00/h1/mwilensk/RFI_Diagnostic_Diffuse_2015/')
+                             '/nfs/eor-00/h1/mwilensk/RFI_Diagnostic_Golden_Set/Digital_Gain_Comparison/')
 else:
     print('I already processed obs ' + str(obslist[args.id - 1]))
