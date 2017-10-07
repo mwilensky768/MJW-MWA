2f4bbe4fe76378db121a16b67a3eea79e31c7025
Mike Wilensky <mjw768@uw.edu>
2017-10-07 13:29:09 -0700

    Adjusted fitting so that nonzero bins are not used for variance calculation

--- Call_Catalog_Run.sh
+++ Call_Catalog_Run.sh
@@ -6,4 +6,4 @@
 echo JOBID ${JOB_ID}
 echo TASKID ${SGE_TASK_ID}
 
-python /nfs/eor-00/h1/mwilensk/MJW-MWA/Catalog_Run.py ${SGE_TASK_ID}
+python /nfs/eor-00/h1/mwilensk/MJW-MWA/Temp_Study.py ${SGE_TASK_ID}
