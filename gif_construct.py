0b39003042207df091070d1e458e480a19466bfb
Mike Wilensky <mjw768@uw.edu>
2017-11-03 20:06:12 -0700

    Going to contrcut chirp ant_pols with clipped colorbar

--- gif_construct.py
+++ gif_construct.py
@@ -1,5 +1,4 @@
-from images2gif import writeGif
-from PIL import Image
+import imageio
 import glob
 
 inpath = '/Users/mike_e_dubs/MWA/Catalogs/Golden_Set_8s_Autos/Ant_Scatter/Narrowband/'
@@ -10,6 +9,6 @@ with open(obslist_path) as f:
     obslist = f.read().split("\n")
 
 for obs in obslist:
-    im_list = glob.glob(inpath + obs + '*')
-    images = [Image.open(im) for im in im_list]
-    writeGif(outpath + obs + '.gif', images, duration=30)
+    im_list = sorted(glob.glob(inpath + obs + '*'), key=lambda name: int(name[104:-4]))
+    images = [imageio.imread(im) for im in im_list]
+    imageio.mimsave(outpath + obs + '.gif', images, duration=30)
