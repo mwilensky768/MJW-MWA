a589a8ea9b3c41b2108400d3a62c33bd1bba2a4c
Mike Wilensky <mjw768@uw.edu>
2017-11-09 17:41:10 -0800

    Made some gifs

--- Auxiliary_Scripts/gif_construct.py
+++ Auxiliary_Scripts/gif_construct.py
@@ -1,19 +1,20 @@
 import imageio
 import glob
 
-inpath = '/Users/mike_e_dubs/MWA/Catalogs/S2_Zenith_Calcut_8s_Autos/Ant_Scatter/Chirp/'
-outpath = '/Users/mike_e_dubs/MWA/Animations/S2_Zenith_Calcut_8s_Autos/Ant_Scatter/Chirp/'
+inpath = '/Users/mike_e_dubs/MWA/Catalogs/Golden_Set_8s_Autos/Vis_Avg/Narrowband/'
+outpath = '/Users/mike_e_dubs/MWA/Animations/Golden_Set_8s_Autos/Vis_Avg/Narrowband/'
+obslist_path = '/Users/mike_e_dubs/MWA/Obs_Lists/Golden_Set_Narrowband_OBSIDS.txt'
+duration = 0.2
 
-obslist_path = '/Users/mike_e_dubs/MWA/Obs_Lists/S2_Zenith_Calcut_8s_Autos_Chirp.txt'
 with open(obslist_path) as f:
     obslist = f.read().split("\n")
+obslist.remove('')
 
-pols = ['XX', 'YY', 'XY', 'YX']
 im_list = {}
 for obs in obslist:
-    for k in range(4):
-        im_list = sorted(glob.glob('%s%s*pol%i*' % (inpath, obs, k)),
-                         key=lambda name: int(name[name.find('_t') + 2:-4]))
-        images = [imageio.imread(im) for im in im_list]
-        if len(images) > 0:
-            imageio.mimsave('%s%s_%s.gif' % (outpath, obs, pols[k]), images, duration=0.5)
+    im_list = sorted(glob.glob('%s%s*' % (inpath, obs)),
+                     key=lambda name: int(name[name.find('_t') + 2:-4]))
+    images = [imageio.imread(im) for im in im_list]
+    if len(images) > 0:
+        imageio.mimsave('%s%s.gif' % (outpath, obs), images,
+                        duration=duration)
