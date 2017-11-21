e5696ce881ccf25bb26ee3420cf0cf0d755e5830
Mike Wilensky <mjw768@uw.edu>
2017-11-21 15:56:58 -0800

    Preparing DS Funky to be vis_avg

--- write_paths_list.sh
+++ write_paths_list.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-obs_file_name='/nfs/eor-00/h1/mwilensk/Diffuse_2015_8s_Autos/Diffuse_2015_Good_Pointings_Misflags.txt'
-obs_path_file_name='/nfs/eor-00/h1/mwilensk/Diffuse_2015_8s_Autos/Diffuse_2015_Good_Pointings_Misflags_paths.txt'
+obs_file_name='/nfs/eor-00/h1/mwilensk/Diffuse_2015_8s_Autos/Diffuse_2015_Good_Pointings_Funky.txt'
+obs_path_file_name='/nfs/eor-00/h1/mwilensk/Diffuse_2015_8s_Autos/Diffuse_2015_Good_Pointings_Funky_paths.txt'
 N_lines=$(wc -l ${obs_file_name})
 
-for i in {1..6}
+for i in {1..15}
 do
 	obs=$(sed -n "${i}p" ${obs_file_name})
 	obs_path="$(python /nfs/eor-00/h1/mwilensk/MWA_Tools/scripts/read_uvfits_loc.py -v 5 -s 1 -o ${obs})"
