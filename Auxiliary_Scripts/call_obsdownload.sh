f943edd55dc85d77238c395d42cf6e103e287826
Mike Wilensky <mjw768@uw.edu>
2018-02-23 14:27:03 -0800

    Modified for PPD analysis

--- Auxiliary_Scripts/call_obsdownload.sh
+++ Auxiliary_Scripts/call_obsdownload.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-obs_file_name='/nfs/eor-00/h1/mwilensk/P2_Bad_Obs/badobs_list_wenyang.txt'
+obs_file_name='/nfs/eor-00/h1/mwilensk/PPD/PPD_Obs.txt'
 
-for i in {2..16}
+for i in {1..29}
 do
   obs=$(sed -n "${i}p" ${obs_file_name})
-  python /nfs/eor-00/h1/mwilensk/MWA_Tools/scripts/obsdownload.py -u -o ${obs}
+  python /nfs/eor-00/h1/mwilensk/MWA_Tools/scripts/obsdownload.py -m -o ${obs}
 done
