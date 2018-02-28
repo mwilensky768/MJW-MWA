a618533c288c33919cde86e65178164178d37850
Mike Wilensky <mjw768@uw.edu>
2018-02-28 11:54:29 -0800

    Need to re-run ppd plot?

--- Auxiliary_Scripts/Call_plotppd.sh
+++ Auxiliary_Scripts/Call_plotppd.sh
@@ -5,5 +5,5 @@ obs_file_name='/nfs/eor-00/h1/mwilensk/PPD/PPD_Obs.txt'
 for i in {1..29}
 do
   obs=$(sed -n "${i}p" ${obs_file_name})
-  python /nfs/eor-00/h1/mwilensk/MWA_Tools/scripts/plotppds.py /nfs/eor-14/r1/EoRuvfits/PPD_clip_metafits/${obs}/${obs}_metafits_ppds.fits
+  python /nfs/eor-00/h1/mwilensk/MWA_Tools/scripts/plotppds.py /nfs/eor-14/r1/EoRuvfits/PPD_clip_metafits/${obs}/${obs}_metafits_ppds.fits -f /nfs/eor-00/h1/mwilensk/PPD/${obs}.png
 done
