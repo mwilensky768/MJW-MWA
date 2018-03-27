Fixed a factor of 2 in gaussian histogram calculation code

Catalog_Funcs.py
@@ -190,8 +190,7 @@ def waterfall_catalog(RFI, outpath, band={}, fit_type={},
                                     xticklabels=xticklabels, mask_color='white')
 
             plt.tight_layout()
-            fig.savefig('%s%s_waterfall_%s.png' %
-                        (figpath, RFI.obs, flag_slice))
+            fig.savefig('%s%s_waterfall_%s.png' % (figpath, RFI.obs, flag_slice))
 
             plt.close(fig)
 
@@ -466,28 +465,30 @@ def bl_scatter_catalog(RFI, outpath, mask, vmin=None, vmax=None, cmap=cm.plasma)
     You must have at least numpy 1.13 to run this!
     """
     ind = np.where(mask)
-    ind_arr = np.unique(np.vstack((ind[0:2])), axis=0)
 
-    for k in range(len(ind[0])):
+    ind_arr = np.unique(np.vstack((ind[0:3])), axis=1)
+
+    for k in range(ind_arr.shape[1]):
         fig, ax = ax_constructor(RFI)
         fig.suptitle('Baseline Scatter Plot t = %i, f = %.1f Mhz' %
-                     (ind[0][k], RFI.UV.freq_array[ind[1][k], ind[2][k]] * 10**(-6)))
-        figpath = '%s/spw%i/' % (outpath, ind[1][k])
-        if not os.path.exists:
+                     (ind_arr[0][k], RFI.UV.freq_array[ind_arr[1][k], ind_arr[2][k]] * 10**(-6)))
+        figpath = '%sAll/spw%i/figs/' % (outpath, ind[1][k])
+        if not os.path.exists(figpath):
             os.makedirs(figpath)
         for m, pol in enumerate(RFI.pols):
             curr_ax = ax_chooser(RFI, ax, m)
             if vmin is None:
-                vmin = np.absolute(RFI.data_array[ind[0][k], :, ind[1][k], ind[2][k], m])[np.absolute(RFI.data_array[ind[0][k], :, ind[1][k], ind[2][k], m]) > 0].min()
+                vmin = np.absolute(RFI.UV.data_array[ind_arr[0][k], :, ind_arr[1][k], ind_arr[2][k], m])[np.absolute(RFI.UV.data_array[ind_arr[0][k], :, ind_arr[1][k], ind_arr[2][k], m]) > 0].min()
             if vmax is None:
-                vmax = np.absolute(RFI.data_array[ind[0][k], :, ind[1][k], ind[2][k], m]).max()
+                vmax = np.absolute(RFI.UV.data_array[ind_arr[0][k], :, ind_arr[1][k], ind_arr[2][k], m]).max()
             plot_lib.scatter_plot_2d(fig, curr_ax,
-                                     RFI.UV.uvw_array[ind[0][k] * RFI.UV.Nbls:ind[0][k] * (RFI.UV.Nbls + 1), 0],
-                                     RFI.UV.uvw_array[ind[0][k] * RFI.UV.Nbls:ind[0][k] * (RFI.UV.Nbls + 1), 1],
+                                     RFI.UV.uvw_array[ind_arr[0][k] * RFI.UV.Nbls:(ind_arr[0][k] + 1) * RFI.UV.Nbls, 0],
+                                     RFI.UV.uvw_array[ind_arr[0][k] * RFI.UV.Nbls:(ind_arr[0][k] + 1) * RFI.UV.Nbls, 1],
                                      title=pol, xlabel='$\lambda u$ (m)', ylabel='$\lambda v$ (m)',
-                                     c=np.absolute(RFI.data_array[ind[0][k], :, ind[1][k], ind[2][k], m]),
+                                     c=np.absolute(RFI.UV.data_array[ind_arr[0][k], :, ind_arr[1][k], ind_arr[2][k], m]),
                                      cmap=cmap, vmin=vmin, vmax=vmax,
-                                     norm=matplotlib.colors(LogNorm(vmin=vmin, vmax=vmax)),
+                                     norm=matplotlib.colors.LogNorm(vmin=vmin, vmax=vmax),
                                      cbar_label=RFI.UV.vis_units)
 
-        fig.savefig('%s%s_bl_scat_t%i_f%i.png' % (figpath, RFI.obs, ind[0][k], ind[2][k]))
+        fig.savefig('%s%s_bl_scat_t%i_spw%i_f%i.png' % (figpath, RFI.obs, ind_arr[0][k], ind_arr[1][k], ind_arr[2][k]))
+        plt.close(fig)

Test_Scripts/Catalog_Test.py
@@ -4,15 +4,18 @@ import glob
 import numpy as np
 from matplotlib import use
 use('Agg')
-from matplotlib.ticker import FixedLocator, AutoMinorLocator
 
 testfile = '/Users/mike_e_dubs/MWA/Data/uvfits/1061313128.uvfits'
-outpath = '/Users/mike_e_dubs/MWA/Test_Plots/1061313238_new_test/'
+outpath = '/Users/mike_e_dubs/MWA/Test_Plots/1061313128_pickle_test/'
 flag_slices = ['All', 'Unflagged']
 band = {'All': [2e3, 1e5], 'Unflagged': [2e3, 1e5]}
 fit_type = {'All': 'rayleigh', 'Unflagged': 'rayleigh'}
 
 RFI = rfi.RFI('1061313128', testfile, auto_remove=True, bad_time_indices=[0, -1])
 
-cf.waterfall_catalog(RFI, outpath, band=band, fit_type=fit_type, bin_window=[0, 2e3])
 cf.INS_catalog(RFI, outpath, flag_slices=flag_slices)
+writepath = '%sAll/all_spw/arrs/' % (outpath)
+base = '%s%s_All_Amp' % (writepath, RFI.obs)
+INS = np.ma.load('%s_INS.npym' % (base))
+
+cf.bl_scatter_catalog(RFI, outpath, INS.mask)