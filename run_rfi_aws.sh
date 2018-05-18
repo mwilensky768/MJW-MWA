50dff3cec0252d11c422e346218fc767de060f3d
Mike Wilensky <mjw768@uw.edu>
2018-05-18 11:30:20 -0700

    AWS debugging

--- run_rfi_aws.sh
+++ run_rfi_aws.sh
@@ -96,7 +96,7 @@ fi
 
 if [ -z ${s3_path} ]
 then
-    s3_path=s3://mwa/golden_set
+    s3_path=s3://mwa-data/golden_set
     echo Using default S3 location: $s3_path
 else
     #strip the last / if present in output directory filepath
