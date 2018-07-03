186c49caf35e1a9d11e98d7bca9ecf8baafd9b3d
Mike Wilensky <mjw768@uw.edu>
2018-07-03 11:14:37 -0700

    Removed junk scripts in main folder and aux folder

--- Auxiliary_Scripts/Digital_Gain_Run_Local.py
+++
@@ -1,5 +0,0 @@
-import rfipy as rfi
-
-RFI = rfi.RFI(1061313008, '/Users/mike_e_dubs/python_stuff/uvfits/1061313008.uvfits')
-
-RFI.digital_gain_compare('/Users/mike_e_dubs/python_stuff/RFI_Diagnostic/Dig_Gain_Plots/')
--- Auxiliary_Scripts/Pol_Writeout.py
+++
@@ -1,9 +0,0 @@
-import pyuvdata
-
-UV = pyuvdata.UVData()
-
-UV.read_uvfits('/Users/mike_e_dubs/python_stuff/smaller_uvfits/f1061313008.uvfits')
-
-UV.select(polarizations=[-5, ])
-
-UV.write_uvfits('/Users/mike_e_dubs/python_stuff/smaller_uvfits/pf1061313008.uvfits')
