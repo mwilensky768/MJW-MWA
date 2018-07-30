a25ac7bc0a630f79410fb48c5aca8e3af2ff4376
Mike Wilensky <mjw768@uw.edu>
2018-07-30 15:12:36 -0700

    Fixed one more Final? error

--- Samp_Thresh_Scripts/LST_Writeout.py
+++ Samp_Thresh_Scripts/LST_Writeout.py
@@ -8,9 +8,9 @@ parser.add_argument('outdir')
 args = parser.parse_args
 
 UV = UVData()
-UV.read(infile, file_type='uvfits', read_data=False)
+UV.read(args.infile, file_type='uvfits', read_data=False)
 lst_arr = np.unique(UV.lst_array)[1:-3]
 times_arr = np.unique(UV.times_arr)[1:-3]
-obs = infile[8:18]
-np.save('%s/%s_lst_arr.npy' % (outdir, obs), lst_arr)
-np.save('%s/%s_times_arr.npy' % (outdir, obs), times_arr)
+obs = args.infile[8:18]
+np.save('%s/%s_lst_arr.npy' % (args.outdir, obs), lst_arr)
+np.save('%s/%s_times_arr.npy' % (args.outdir, obs), times_arr)
