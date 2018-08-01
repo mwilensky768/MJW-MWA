637c4303f9a03cc7077969be0a82befb364a1d7b
Mike Wilensky <mjw768@uw.edu>
2018-08-01 16:18:45 -0700

    Had to *flag* channels, not downselect

--- Long_Run/TV_Split.py
+++ Long_Run/TV_Split.py
@@ -19,12 +19,25 @@ freq_chans = {6: range(87, 174),
 
 for channel in args.channels:
     UV_TV = UV.select(times=np.unique(UV.time_array)[min(args.TV_ind):max(args.TV_ind)],
-                      freq_chans=freq_chans[channel],
                       inplace=False)
+    ind = np.where(UV_TV.nsample_array == 0)
+    for m in range(len(ind[0])):
+        UV_TV.nsample_array[ind[0][m], ind[1][m], ind[2][m], ind[3][m]] = \
+            UV_TV.nsample_array[0, ind[1][m], ind[2][m] % 16, ind[3][m]]
+    UV_TV.flag_array[:, :, freq_chans[channel]] = 0
+    UV_TV.flag_array[:, :, :min(freq_chans[channel])] = 1
+    UV_TV.flag_array[:, :, max(freq_chans[channel])] = 1
 
     UV_cal = UV.select(times=np.unique(UV.time_array)[min(args.cal_ind):max(args.cal_ind)],
-                       freq_chans=freq_chans[channel],
                        inplace=False)
+    UV_cal.flag_array[:, :, freq_chans[channel]] = 0
+    UV_cal.flag_array[:, :, :min(freq_chans[channel])] = 1
+    UV_cal.flag_array[:, :, max(freq_chans[channel])] = 1
+
+    ind = np.where(UV_cal.nsample_array == 0)
+    for m in range(len(ind[0])):
+        UV_cal.nsample_array[ind[0][m], ind[1][m], ind[2][m], ind[3][m]] = \
+            UV_cal.nsample_array[0, ind[1][m], ind[2][m] % 16, ind[3][m]]
 
     UV_TV.write_uvfits('%s/%s_TV%i_t%i_t%i.uvfits' %
                        (args.outpath, args.obsid, channel, min(args.TV_ind), max(args.TV_ind)))
