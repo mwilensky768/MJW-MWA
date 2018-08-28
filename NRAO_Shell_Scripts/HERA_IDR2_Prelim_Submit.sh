97f05c1f2447cbcaa0a643c8ee63423da788bd67
Mike Wilensky <mjw768@uw.edu>
2018-08-28 14:53:20 -0700

    Had to rename a variable

--- NRAO_Shell_Scripts/HERA_IDR2_Prelim_Submit.sh
+++ NRAO_Shell_Scripts/HERA_IDR2_Prelim_Submit.sh
@@ -5,7 +5,7 @@ do
   day=${list:2:7}
   for i in {1..5}
   do
-    obsid=$(head -"$i" $file | tail -1)
+    obsid=$(head -"$i" $list | tail -1)
     outdir=/lustre/aoc/projects/hera/mwilensk/HERA_IDR2_Prelim_Set
     for pol in xx yy xy yx
     do
