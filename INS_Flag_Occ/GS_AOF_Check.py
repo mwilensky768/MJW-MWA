f6112c69f7cb083c8ef4030384a8c3fa78b2a062
Mike Wilensky <mjw768@uw.edu>
2018-08-29 23:18:55 -0700

    too many indices for array

--- INS_Flag_Occ/GS_AOF_Check.py
+++ INS_Flag_Occ/GS_AOF_Check.py
@@ -10,7 +10,7 @@ args = parser.parse_args()
 
 UV = UVData()
 UV.read(args.infile, type='uvfits', ant_str='cross')
-UV.select(times=np.unique(time_array)[0, -1, -2, -3])
+UV.select(times=np.unique(time_array)[[0, -1, -2, -3]])
 edges = [0 + 16 * i for i in range(24)] + [15 + 16 * k for k in range(24)]
 UV.flag_array[:, :, edges] = 0
 num = UV.flag_array.sum()
