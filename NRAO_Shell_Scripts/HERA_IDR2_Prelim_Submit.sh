c3245601180ade966f12b0eab71cdf9ccdb42121
Mike Wilensky <mjw768@uw.edu>
2018-09-11 16:15:33 -0700

    outdir issues

--- NRAO_Shell_Scripts/HERA_IDR2_Prelim_Submit.sh
+++ NRAO_Shell_Scripts/HERA_IDR2_Prelim_Submit.sh
@@ -7,12 +7,12 @@ do
   do
     obsid=$(head -"$i" /lustre/aoc/projects/hera/mwilensk/Obs_Lists/Obs_Select/$list | tail -1)
     echo $obsid
-    outdir=/lustre/aoc/projects/hera/mwilensk/HERA_IDR2_Prelim_Set_OR_original
+    outdir=/lustre/aoc/projects/hera/mwilensk/HERA_IDR2_Prelim_Set_nocut
     for pol in xx yy xy yx
     do
       obs=${obsid}.${pol}.HH
       echo $obs
-      indir=/lustre/aoc/projects/hera/H1C_IDR2/${day}/${obs}.uvOR
+      indir=/lustre/aoc/projects/hera/H1C_IDR2/${day}/${obs}.uv
       python /lustre/aoc/projects/hera/mwilensk/MJW-MWA/Catalog_Gen.py $obs $indir $outdir
     done
   done
