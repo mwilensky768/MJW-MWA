b342d19ad966f676dd4ef90c0413d05d12bf6704
Mike Wilensky <mjw768@uw.edu>
2018-09-12 15:10:48 -0700

    Change shell script style

--- call_lin_grad_compare.sh
+++ call_lin_grad_compare.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+basedir=/lustre/aoc/projects/hera/mwilensk/HERA_IDR2_Prelim_Set_nocut
+outdir=${basedir}_compare
+obsfile=/lustre/aoc/projects/hera/mwilensk/HERA_IDR2_Prelim_obs.txt
+flag_choice=None
+
 for order in {0..5}
 do
   python /lustre/aoc/projects/hera/mwilensk/MJW-MWA/MS_line_grad/INS_lin_grad_compare.py $basedir ${outdir}_${order} $obsfile $flag_choice $order
