de588ff24331626245efd632177bacdb2243abb7
Mike Wilensky <mjw768@uw.edu>
2018-09-12 14:27:13 -0700

    outdir mod

--- call_lin_grad_compare.sh
+++ call_lin_grad_compare.sh
@@ -2,5 +2,5 @@
 
 for order in {0..5}
 do
-  /lustre/aoc/projects/hera/mwilensk/MJW-MWA/MS_line_grad/INS_lin_grad_compare.py $basedir $outdir $obsfile $flag_choice $order
+  /lustre/aoc/projects/hera/mwilensk/MJW-MWA/MS_line_grad/INS_lin_grad_compare.py $basedir ${outdir}_${order} $obsfile $flag_choice $order
 done
