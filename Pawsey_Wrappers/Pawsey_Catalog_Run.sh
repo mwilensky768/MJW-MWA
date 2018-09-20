42f88c0e5018988c183a335154f19b3c9916d5ea
Mike Wilensky <mjw768@uw.edu>
2018-09-19 17:04:02 -0700

    It didn't like my wildcard

--- Pawsey_Wrappers/Pawsey_Catalog_Run.sh
+++ Pawsey_Wrappers/Pawsey_Catalog_Run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-cotter -o ${datadir}/${obs}_noavg_noflag.uvfits -m ${data_dir}/${obs}/${obs}_metafits_ppds.fits -timeres 0.5 -freqres 40 -norfi -noflagdcchannels -edgewidth 0 -initflag 0 $(ls ${data_dir}/${obs}/*gpubox*)
+gpufiles=$(ls ${data_dir}/${obs}/*gpubox*)
+cotter -o ${datadir}/${obs}_noavg_noflag.uvfits -m ${data_dir}/${obs}/${obs}_metafits_ppds.fits -timeres 0.5 -freqres 40 -norfi -noflagdcchannels -edgewidth 0 -initflag 0 $gpufiles
 
 python /home/mwilensk/MJW-MWA/Catalog_Run.py $obs ${data_dir}/${obs}_noavg_noflag.uvfits $outdir
