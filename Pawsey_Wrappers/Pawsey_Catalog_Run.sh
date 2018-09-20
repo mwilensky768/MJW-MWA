afb316c7ef782bd44159465d2c2d1b42de2676c5
Mike Wilensky <mjw768@uw.edu>
2018-09-19 17:12:41 -0700

    bing bang

--- Pawsey_Wrappers/Pawsey_Catalog_Run.sh
+++ Pawsey_Wrappers/Pawsey_Catalog_Run.sh
@@ -13,4 +13,4 @@ data_dir=/astro/mwaeor/MWA/data
 gpufiles=$(ls ${data_dir}/${obs}/*gpubox*)
 cotter -o ${datadir}/${obs}_noavg_noflag.uvfits -m ${data_dir}/${obs}/${obs}_metafits_ppds.fits -timeres 0.5 -freqres 40 -norfi -noflagdcchannels -edgewidth 0 -initflag 0 $gpufiles
 
-python /home/mwilensk/MJW-MWA/Catalog_Run.py $obs ${data_dir}/${obs}_noavg_noflag.uvfits $outdir
+python /home/mwilensk/MJW-MWA/Catalog_Gen.py $obs ${data_dir}/${obs}_noavg_noflag.uvfits $outdir
