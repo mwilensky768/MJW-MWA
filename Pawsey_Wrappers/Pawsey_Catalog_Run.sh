106d9543dab2fb103792f5634da7b2db243fe656
Mike Wilensky <mjw768@uw.edu>
2018-09-19 17:14:51 -0700

    kajsmklfamsdlkgm

--- Pawsey_Wrappers/Pawsey_Catalog_Run.sh
+++ Pawsey_Wrappers/Pawsey_Catalog_Run.sh
@@ -11,6 +11,6 @@ module load cotter
 data_dir=/astro/mwaeor/MWA/data
 
 gpufiles=$(ls ${data_dir}/${obs}/*gpubox*)
-cotter -o ${datadir}/${obs}_noavg_noflag.uvfits -m ${data_dir}/${obs}/${obs}_metafits_ppds.fits -timeres 0.5 -freqres 40 -norfi -noflagdcchannels -edgewidth 0 -initflag 0 $gpufiles
+cotter -o ${data_dir}/${obs}_noavg_noflag.uvfits -m ${data_dir}/${obs}/${obs}_metafits_ppds.fits -timeres 0.5 -freqres 40 -norfi -noflagdcchannels -edgewidth 0 -initflag 0 $gpufiles
 
-python /home/mwilensk/MJW-MWA/Catalog_Gen.py $obs ${data_dir}/${obs}_noavg_noflag.uvfits $outdir
+python /home/mwilensky/MJW-MWA/Catalog_Gen.py $obs ${data_dir}/${obs}_noavg_noflag.uvfits $outdir
