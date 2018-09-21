cec24ba160f005e10838c164d09c558b74dfcffa
Mike Wilensky <mjw768@uw.edu>
2018-09-21 11:19:51 -0700

    Trying to work out memory requiremments ask

--- Pawsey_Wrappers/Pawsey_Catalog_Run.sh
+++ Pawsey_Wrappers/Pawsey_Catalog_Run.sh
@@ -10,5 +10,4 @@ module load pyuvdata/master
 module load cotter
 data_dir=/astro/mwaeor/MWA/data
 
-python /home/mwilensky/MJW-MWA/Catalog_Gen.py $obs ${data_dir}/${obs}/${obs}_noavg_noflag.uvfits ${outdir}_0_112 --time_range 112 224
-python /home/mwilensky/MJW-MWA/Catalog_Gen.py $obs ${data_dir}/${obs}/${obs}_noavg_noflag.uvfits ${outdir}_112_224 --time_range 0 112
+python /home/mwilensky/MJW-MWA/Catalog_Gen.py $obs ${data_dir}/${obs}/${obs}_noavg_noflag.uvfits ${outdir}
