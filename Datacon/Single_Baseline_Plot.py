96e4055784390dea2eb7cb5b2a49eb0bc6edc5ec
Mike Wilensky <mjw768@uw.edu>
2018-09-24 14:22:34 -0700

    finishing touches

--- Datacon/Single_Baseline_Plot.py
+++ Datacon/Single_Baseline_Plot.py
@@ -22,6 +22,8 @@ UV.data_array = UV.data_array.reshape([UV.Ntimes, UV.Nbls, UV.Nspws, UV.Nfreqs,
 UV.data_array = UV.data_array[1:-3]
 
 ylabel = ['Time (2s)', 'Time Pair']
+tag = ['vis', 'diff']
+title = ['MWA Single Baseline Visibilities', 'MWA Single Baseline Visibility Differences']
 for i in range(UV.Nbls):
     im_dat = UV.data_array[:, i, 0, :, 0]
     for k in range(2):
@@ -31,17 +33,17 @@ for i in range(UV.Nbls):
             title = 'MWA Single Baseline Visibility Differences'
         for label in ['Real', 'Imag']:
             fig, ax = plt.subplots(figsize=(14, 8))
-            plot_lib.image_plot(fig, ax, getattr(im_dat, label.lower()), ylabel=ylabel[i],
+            plot_lib.image_plot(fig, ax, getattr(im_dat, label.lower()), ylabel=ylabel[k],
                                 cmap=cm.coolwarm,
                                 title='%s (%s)' % (title, label),
                                 freq_array=UV.freq_array[0], cbar_label=UV.vis_units)
-            fig.savefig('%s/%s_%i_%s.png' % (args.outpath, args.obs, i, label))
+            fig.savefig('%s/%s_%i_%s_%s.png' % (args.outpath, args.obs, i, tag[k], label))
             plt.close(fig)
         fig, ax = plt.subplots(figsize=(14, 8))
         plot_lib.image_plot(fig, ax, np.absolute(im_dat), cbar_label=UV.vis_units,
                             title='%s (Amplitude)' % title,
-                            freq_array=UV.freq_array[0], ylabel=ylabel[i])
-        fig.savefig('%s/%s_%i_amplitude.png' % (outdir, args.outpath, i))
+                            freq_array=UV.freq_array[0], ylabel=ylabel[k])
+        fig.savefig('%s/%s_%i_%s_amplitude.png' % (args.outpath, args.obs, i, tag[k]))
         plt.close(fig)
     UV.data_array = np.absolute(np.diff(UV.data_array, axis=0))
     im_dat = np.absolute(im_dat)
