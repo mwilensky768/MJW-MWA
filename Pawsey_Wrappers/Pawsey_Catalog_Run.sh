318e054b86fe4a6634b76feb0a19626edd578595
Mike Wilensky <mjw768@uw.edu>
2018-09-28 03:34:19 +0800

    Merge conflict incoming

--- Pawsey_Wrappers/Pawsey_Catalog_Run.sh
+++ Pawsey_Wrappers/Pawsey_Catalog_Run.sh
@@ -11,7 +11,7 @@ module load cotter
 data_dir=/astro/mwaeor/MWA/data
 
 
-gpufiles=$(ls ${data_dir}/${obs}/*gpubox*)
-cotter -o ${data_dir}/${obs}/${obs}_80khz_noflag.uvfits -m ${data_dir}/${obs}/${obs}_metafits_ppds.fits -timeres 0.5 -freqres 80 -norfi -noflagdcchannels -edgewidth 0 -initflag 0 $gpufiles
+# gpufiles=$(ls ${data_dir}/${obs}/*gpubox*)
+# cotter -o ${data_dir}/${obs}/${obs}_80khz_noflag.uvfits -m ${data_dir}/${obs}/${obs}_metafits_ppds.fits -timeres 0.5 -freqres 80 -norfi -noflagdcchannels -edgewidth 0 -initflag 0 $gpufiles
 
 python /home/mwilensky/MJW-MWA/Catalog_Gen.py $obs ${data_dir}/${obs}/${obs}_80khz_noflag.uvfits ${outdir}
