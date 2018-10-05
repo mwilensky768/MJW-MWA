9aee2f797d58f3061f008f3529510ceaed410599
Mike Wilensky <mjw768@uw.edu>
2018-10-05 11:52:25 -0700

    Asked for a whole day

--- INS_Flag_Occ/GS_Concatenate.py
+++ INS_Flag_Occ/GS_Concatenate.py
@@ -3,14 +3,23 @@ from SSINS import util
 import glob
 
 basedir = '/Users/mike_e_dubs/MWA/INS/Golden_Set_80khz_0.5s'
-obslist_dir
-suff = ['0_56', '56_112', '112_168', '168_224']
-L = len('%s/GS_SSINS_0_56/' % basedir)
-pathlist = glob.glob('%s/GS_SSINS_0_56/*data.npy')
+suff = ['0_96', '96_192', '192_288', '288_384']
+L = len('%s/GS_SSINS_0_96_80khz/arrs/' % basedir)
+pathlist = glob.glob('%s/GS_SSINS_0_96_80khz/arrs/*data.npym' % basedir)
+
 obslist = []
 for path in pathlist:
     obslist.append(path[L:L + 10])
 
 for obs in obslist:
-    data = np
-for i in np.arange(4):
+    for i in np.arange(4):
+        data_path = '%s/GS_SSINS_%s_80khz/arrs/%s_None_INS_data.npym' % (basedir, suff[i], obs)
+        Nbls_path = '%s/GS_SSINS_%s_80khz/arrs/%s_None_INS_Nbls.npym' % (basedir, suff[i], obs)
+        if not i:
+            data = np.load(data_path)
+            Nbls = np.load(Nbls_path)
+        else:
+            data = np.concatenate((data, np.load(data_path)), axis=2)
+            Nbls = np.concatenate((Nbls, np.load(Nbls_path)), axis=2)
+    np.save('%s/GS_SSINS_concat/arrs/%s_None_INS_data.npym' % (basedir, obs), data)
+    np.save('%s/GS_SSINS_concat/arrs/%s_None_INS_Nbls.npym' % (basedir, obs), Nbls)
