50253d45a5ccfdc7764d86fa1d2801d963993458
Mike Wilensky <mjw768@uw.edu>
2018-10-08 17:54:58 -0700

    fixed references

--- Pawsey_Wrappers/Pawsey_Catalog_Run.sh
+++ Pawsey_Wrappers/Pawsey_Catalog_Run.sh
@@ -14,6 +14,6 @@ data_dir=/astro/mwaeor/MWA/data
 # gpufiles=$(ls ${data_dir}/${obs}/*gpubox*)
 gpufiles=$(ls ${data_dir}/${obs}/*gpubox*${t_int}.fits)
 echo $gpufiles >> SSINS_%j.out
-cotter -o ${data_dir}/${obs}/SSINS_uvfits/${obs}_noavg_noflag_${box_num}.uvfits -m ${data_dir}/${obs}/${obs}_metafits_ppds.fits -timeres 0.5 -freqres 40 -norfi -noflagdcchannels -edgewidth 0 -initflag 0 -allowmissing $gpufiles
+cotter -o ${data_dir}/${obs}/SSINS_uvfits/${obs}_noavg_noflag_${t_int}.uvfits -m ${data_dir}/${obs}/${obs}_metafits_ppds.fits -timeres 0.5 -freqres 40 -norfi -noflagdcchannels -edgewidth 0 -initflag 0 -allowmissing $gpufiles
 
-python /home/mwilensky/MJW-MWA/Catalog_Gen.py $obs ${data_dir}/${obs}/SSINS_uvfits/${obs}_noavg_noflag_${box_num}.uvfits ${outdir}_${box_num}_noavg
+python /home/mwilensky/MJW-MWA/Catalog_Gen.py $obs ${data_dir}/${obs}/SSINS_uvfits/${obs}_noavg_noflag_${t_int}.uvfits ${outdir}_${t_int}_noavg
