712aedafadc6b33b828d9e59a21b72983175f12b
Mike Wilensky <mjw768@uw.edu>
2018-10-08 17:53:16 -0700

    do time integration separation instead of

--- Pawsey_Wrappers/Pawsey_COTTER_SSINS_Wrapper.sh
+++ Pawsey_Wrappers/Pawsey_COTTER_SSINS_Wrapper.sh
@@ -13,8 +13,8 @@ done
 
 while read obs
 do
-  for box_num in 0{1..9} {10..24}
+  for t_int in 01 02
   do
-    sbatch --nodes=1 --mem=60000 --time=12:00:00 --account=mwaeor --job-name=SSINS --output=SSINS_%j.out --error=SSINS_%j.e --export=obs=$obs,outdir=$outdir,box_num=$box_num /home/mwilensky/MJW-MWA/Pawsey_Wrappers/Pawsey_Catalog_Run.sh
+    sbatch --nodes=1 --mem=60000 --time=12:00:00 --account=mwaeor --job-name=SSINS --output=SSINS_%j.out --error=SSINS_%j.e --export=obs=$obs,outdir=$outdir,t_int=$t_int /home/mwilensky/MJW-MWA/Pawsey_Wrappers/Pawsey_Catalog_Run.sh
   done
 done < $obs_file_name
