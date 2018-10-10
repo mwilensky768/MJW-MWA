496e4ed4b8758fb3fb33c57f3ac2157b95912d32
Mike Wilensky <mjw768@uw.edu>
2018-10-10 09:22:58 -0700

    run on zeus

--- Pawsey_Wrappers/Pawsey_COTTER_SSINS_Wrapper.sh
+++ Pawsey_Wrappers/Pawsey_COTTER_SSINS_Wrapper.sh
@@ -13,8 +13,5 @@ done
 
 while read obs
 do
-  for t_int in 00 01
-  do
-    sbatch --nodes=1 --mem=60000 --time=12:00:00 --account=mwaeor --job-name=SSINS --output=SSINS_%j.out --error=SSINS_%j.e --export=obs=$obs,outdir=$outdir,t_int=$t_int /home/mwilensky/MJW-MWA/Pawsey_Wrappers/Pawsey_Catalog_Run.sh
-  done
+  sbatch --nodes=1 --mem=120000 --time=12:00:00 --account=mwaeor --job-name=SSINS --output=SSINS_%j.out --error=SSINS_%j.e --export=obs=$obs,outdir=$outdir /home/mwilensky/MJW-MWA/Pawsey_Wrappers/Pawsey_Catalog_Run.sh
 done < $obs_file_name
