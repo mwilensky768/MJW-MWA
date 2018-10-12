d2e9fd4074cca77d36e83ec221da0fedddbb4195
Mike Wilensky <mjw768@uw.edu>
2018-10-12 12:09:13 -0700

    require fits file extension for gpubox files

--- Pawsey_Wrappers/Pawsey_Catalog_Run.sh
+++ Pawsey_Wrappers/Pawsey_Catalog_Run.sh
@@ -13,7 +13,7 @@ data_dir=/astro/mwaeor/MWA/data
 
 if [ ! -e ${data_dir}/${obs}/${obs}_noavg_noflag.uvfits ]; then
   echo "Executing COTTER"
-  gpufiles=$(ls ${data_dir}/${obs}/*gpubox*)
+  gpufiles=$(ls ${data_dir}/${obs}/*gpubox*.fits)
   cotter -o ${data_dir}/${obs}/${obs}_noavg_noflag.uvfits -m ${data_dir}/${obs}/${obs}_metafits_ppds.fits -timeres 0.5 -freqres 40 -norfi -noflagdcchannels -edgewidth 0 -initflag 0 -allowmissing $gpufiles
 fi
 
