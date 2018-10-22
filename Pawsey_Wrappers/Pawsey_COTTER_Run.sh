365ea7aca38ea5683e5b56c1545694bfc6e53525
Mike Wilensky <mjw768@uw.edu>
2018-10-22 15:27:39 +1100

    sbcount 1

--- Pawsey_Wrappers/Pawsey_COTTER_Run.sh
+++ Pawsey_Wrappers/Pawsey_COTTER_Run.sh
@@ -4,9 +4,9 @@ module use /group/mwa/software/modulefiles
 module load cotter
 data_dir=/astro/mwaeor/MWA/data
 
-if [ ! -e ${data_dir}/${obs}/${obs}_noflag.uvfits ]; then
+if [ ! -e ${data_dir}/${obs}/${obs}_noflag_box1.uvfits ]; then
   echo $obs
   echo "Executing COTTER"
   gpufiles=$(ls ${data_dir}/${obs}/*gpubox*.fits)
-  cotter -o ${data_dir}/${obs}/${obs}_noflag.uvfits -m ${data_dir}/${obs}/${obs}_metafits_ppds.fits -timeres 2 -freqres 80 -norfi -noflagdcchannels -edgewidth 80 -initflag 2 -endflag 6 -allowmissing $gpufiles
+  cotter -o ${data_dir}/${obs}/${obs}_noflag.uvfits -m ${data_dir}/${obs}/${obs}_metafits_ppds.fits -timeres 0.5 -freqres 40 -norfi -noflagdcchannels -edgewidth 0 -initflag 0 -endflag 0 -allowmissing -sbcount 1 $gpufiles
 fi
