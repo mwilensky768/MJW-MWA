8cc64f972a7b3998c4c74f5587315359fced642a
Mike Wilensky <mjw768@uw.edu>
2018-10-24 13:59:59 +1100

    had to apply the flags

--- MF_flag_write.py
+++ MF_flag_write.py
@@ -15,5 +15,6 @@ ss.MF_prepare(sig_thresh=5, shape_dict={'TV6': [1.74e8, 1.81e8],
                                         'TV8': [1.88e8, 1.95e8]})
 ss.MF.apply_match_test()
 cp.MF_plot(ss.MF)
+ss.apply_flags(choice='INS', INS=ss.INS)
 ss.write('%s/%s.uvfits' % (args.outpath, args.obs), 'uvfits', inpath=args.inpath,
          nsample_default=8, read_kwargs={'ant_str': 'cross'})
