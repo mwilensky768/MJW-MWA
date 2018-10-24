550ab056333d4687a4b3e8f930df0365be310cb5
Mike Wilensky <mjw768@uw.edu>
2018-10-24 12:01:18 +1100

    needed :

--- 1061313128/1061313128_mwaf_map.py
+++ 1061313128/1061313128_mwaf_map.py
@@ -16,6 +16,6 @@ for i in range(1, 25):
     if ntime < min_ntime:
         min_ntime = ntime
     nbl = nant * (nant + 1) / 2
-    flag_arr_seq.append(hdulist[1].data['FLAGS'].reshape([ntime, nbl, nchan])[min_ntime])
+    flag_arr_seq.append(hdulist[1].data['FLAGS'].reshape([ntime, nbl, nchan])[:min_ntime])
 flag_map = np.mean(np.concatenate(flag_arr_seq, axis=2), axis=1)
 np.save('/group/mwaeor/mwilensky/1061313128_flag_map.npy', flag_map)
