771491cf8350686d3470d20f987133457cf5adf6
Mike Wilensky <mjw768@uw.edu>
2018-10-24 12:12:40 +1100

    Odd path error

--- 1061313128/1061313128_flag_rewrite.py
+++ 1061313128/1061313128_flag_rewrite.py
@@ -13,4 +13,4 @@ UV.read(args.inpath)
 slc = slice(-4 * UV.Nbls, -1 * UV.Nbls)
 UV.nsample_array[slc][UV.nsample_array[slc] == 0] = 8
 UV.flag_array[slc] = 1
-UV.write_uvfits('%s/1061313128.uvfits' % args.outpath)
+UV.write_uvfits('%s/1061313128_rewrite.uvfits' % args.outpath)
