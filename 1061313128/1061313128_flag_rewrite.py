8ff39f5b950c2fa007bcfbd9490baa3b3cbcf8a4
Mike Wilensky <mjw768@uw.edu>
2018-10-24 11:45:15 +1100

    small typo

--- 1061313128/1061313128_flag_rewrite.py
+++ 1061313128/1061313128_flag_rewrite.py
@@ -6,7 +6,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument('obs')
 parser.add_argument('inpath')
 parser.add_argument('outpath')
-args = parser.parse-args()
+args = parser.parse_args()
 
 UV = UVData()
 UV.read(args.inpath)
