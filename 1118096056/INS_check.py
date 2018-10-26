661418acb985a3edea704e99eaf04319a73a1c6b
Mike Wilensky <mjw768@uw.edu>
2018-10-26 13:31:25 +1100

    Please work

--- 1118096056/INS_check.py
+++ 1118096056/INS_check.py
@@ -3,3 +3,8 @@ from SSINS import SS
 inpath = '/Users/mike_e_dubs/MWA/Data/uvfits/1118096056.uvfits'
 outpath = '/Users/mike_e_dubs/MWA/1118096056'
 obs = '1118096056'
+
+ss = SS(obs=obs, inpath=inpath, outpath=outpath,
+        read_kwargs={'ant_str': 'cross'})
+ss.INS_prepare()
+ss.INS.save()
