1c46f917ae4f063223495a3575f6c0ec42800a4b
Mike Wilensky <mjw768@uw.edu>
2018-10-26 11:49:24 +1100

    variable name

--- 1118096056/INS_check.py
+++ 1118096056/INS_check.py
@@ -1,3 +1,5 @@
 from SSINS import SS
 
-indir = '/Users/mike_e_dubs/MWA/Data/uvfits/1118096056.uvfits'
+inpath = '/Users/mike_e_dubs/MWA/Data/uvfits/1118096056.uvfits'
+outpath = '/Users/mike_e_dubs/MWA/1118096056'
+obs = '1118096056'
