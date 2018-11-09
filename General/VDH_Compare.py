94afda8ec2df9da4521e54dcf7cbbe21d9d9ed38
Mike Wilensky <mjw768@uw.edu>
2018-11-08 16:14:26 -0800

    oops

--- General/VDH_Compare.py
+++ General/VDH_Compare.py
@@ -4,12 +4,11 @@ from SSINS import Catalog_Plot as cp
 inpath = '/Volumes/Faramir/uvfits'
 obslist = ['1061313128', '1061312640']
 outpath = '/Users/mikewilensky/General/Hist_Compare_Big_Legend'
-read_kwargs = {'ant_str': 'cross',
-               'bad_time_indices': [0, -1, -2, -3]}
+read_kwargs = {'ant_str': 'cross'}
 
 for obs in obslist:
     ss = SS(inpath='%s/%s.uvfits' % (inpath, obs), obs=obs, outpath=outpath,
-            read_kwargs=read_kwargs)
+            read_kwargs=read_kwargs, bad_time_indices=[0, -1, -2, -3])
     ss.VDH_prepare()
     cp.VDH_plot(ss.VDH, leg_size='xx-large')
     del ss
