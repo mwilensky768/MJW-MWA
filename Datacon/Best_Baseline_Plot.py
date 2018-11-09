aba74cf12dbb33cf28b3e8b327ac02cd2972c41a
Mike Wilensky <mjw768@uw.edu>
2018-11-09 13:42:54 -0800

    poop

--- Datacon/Best_Baseline_Plot.py
+++ Datacon/Best_Baseline_Plot.py
@@ -33,7 +33,7 @@ print(data.shape)
 plot_lib.image_plot(fig, ax, data.real, cmap=cm.RdGy_r, freq_array=freq_array,
                     cbar_label='Amplitude (UNCALIB)', aspect=data.shape[1] / (data.shape[0]),
                     ylabel='Time (2s)')
-plot_lib.image_plot(fig_diff, ax_diff, data_diff, freq_array=freq_array, ylabel='Time (2s)'
+plot_lib.image_plot(fig_diff, ax_diff, data_diff, freq_array=freq_array, ylabel='Time (2s)',
                     cbar_label='Amplitude (UNCALIB)', aspect=data.shape[1] / (data.shape[0]))
 fig.savefig('%s/Best_Baseline.png' % outpath)
 fig_diff.savefig('%s/Best_Baseline_Diff.png' % outpath)
