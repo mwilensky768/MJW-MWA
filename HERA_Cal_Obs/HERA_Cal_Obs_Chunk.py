9c9edba525dcbeac8c5ff569da4e4fe25ac4ca28
Mike Wilensky <mjw768@uw.edu>
2018-12-13 15:54:30 -0800

    Added chunking script

---
+++ HERA_Cal_Obs/HERA_Cal_Obs_Chunk.py
@@ -0,0 +1,21 @@
+from pyuvdata import UVData
+import numpy as np
+import argparse
+from SSINS import util as u
+
+parser = argparse.ArgumentParser()
+parser.add_argument('obsfile')
+parser.add_argument('indir')
+parser.add_argument('outdir')
+args = parser.parse_args()
+
+obslist = u.make_obslist(args.obsfile)
+
+for obs in obslist:
+    UV = UVData()
+    UV.read('%s/%s.uvfits' % (args.indir, obs), file_type='uvfits')
+    for chunk in range(6):
+        times = np.unique(UV.time_array)[10 * chunk: 10 * (chunk + 1)]
+        UV1 = UV.select(times=times, inplace=False)
+        UV1.write('%s/%s_%i.uvfits' % (args.outdir, obs, chunk + 1))
+        del UV1
