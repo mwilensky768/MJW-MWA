7e002637e3fad674bff395485ec0155568673953
Mike Wilensky <mjw768@uw.edu>
2018-12-13 16:57:04 -0800

    downstream error

--- HERA_Cal_Obs/HERA_Cal_Obs_Chunk.py
+++ HERA_Cal_Obs/HERA_Cal_Obs_Chunk.py
@@ -15,7 +15,7 @@ for obs in obslist:
     UV = UVData()
     UV.read('%s/%s.uvfits' % (args.indir, obs), file_type='uvfits')
     for chunk in range(12):
-        times = np.unique(UV.time_array)[10 * chunk: 10 * (chunk + 1)]
+        times = np.unique(UV.time_array)[5 * chunk: 5 * (chunk + 1)]
         UV1 = UV.select(times=times, inplace=False)
         UV1.write_uvfits('%s/%s_%i.uvfits' % (args.outdir, obs, chunk + 1))
         del UV1
