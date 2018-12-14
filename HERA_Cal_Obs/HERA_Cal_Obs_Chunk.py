081678bee6073483dfa7579e0c25f512dca683e2
Mike Wilensky <mjw768@uw.edu>
2018-12-13 16:56:01 -0800

    needed smaller chunks

--- HERA_Cal_Obs/HERA_Cal_Obs_Chunk.py
+++ HERA_Cal_Obs/HERA_Cal_Obs_Chunk.py
@@ -14,7 +14,7 @@ obslist = u.make_obslist(args.obsfile)
 for obs in obslist:
     UV = UVData()
     UV.read('%s/%s.uvfits' % (args.indir, obs), file_type='uvfits')
-    for chunk in range(6):
+    for chunk in range(12):
         times = np.unique(UV.time_array)[10 * chunk: 10 * (chunk + 1)]
         UV1 = UV.select(times=times, inplace=False)
         UV1.write_uvfits('%s/%s_%i.uvfits' % (args.outdir, obs, chunk + 1))
