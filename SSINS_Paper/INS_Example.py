bd962eb004a2757f43bf09e3046ae2331710e786
Mike Wilensky <mjw768@uw.edu>
2019-01-18 15:40:55 -0800

    please just let me pass

--- SSINS_Paper/INS_Example.py
+++ SSINS_Paper/INS_Example.py
@@ -9,9 +9,9 @@ obslist = ['1061312640', '1066742016']
 
 for obs in obslist:
     UV = UVData()
-    UV.read('%s/%s.uvfits' % (indir, obs), file_type='uvfits', polarizations=-5, obs=obs)
+    UV.read('%s/%s.uvfits' % (indir, obs), file_type='uvfits', polarizations=-5)
     UV.select(times=np.unique(UV.time_array)[1:-3], ant_str='cross')
-    ss = SS(UV=UV, outpath='/Users/mikewilensky/SSINS_Paper')
+    ss = SS(UV=UV, outpath='/Users/mikewilensky/SSINS_Paper', , obs=obs)
     ss.INS_prepare()
     fig, ax = plt.subplots(figsize=(16, 9))
     plot_lib.image_plot(fig, ax, ss.INS.data[:, 0, :, 0], aspect='auto',
