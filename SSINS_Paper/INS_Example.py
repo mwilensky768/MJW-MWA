3f194af613d2a0a3da3d98fd6247a1d7f56fc3a8
Mike Wilensky <mjw768@uw.edu>
2019-01-18 16:05:54 -0800

    You are a fucking bitch dude

--- SSINS_Paper/INS_Example.py
+++ SSINS_Paper/INS_Example.py
@@ -20,7 +20,7 @@ for obs in obslist:
     fig_ms, ax_ms = plt.subplots(figsize=(16, 9))
     plot_lib.image_plot(fig_ms, ax_ms, ss.INS.data_ms[:, 0, :, 0], aspect='auto',
                         freq_array=UV.freq_array[0], ylabel='Time (2 s)',
-                        xlabel='Frequency (Mhz)', cmap=cm.coolwarm
+                        xlabel='Frequency (Mhz)', cmap=cm.coolwarm,
                         cbar_label='Deviation ($\hat{\sigma}$)')
     fig.savefig('%s/%s_INS_data.pdf' % (ss.outpath, obs))
     fig_ms.savefig('%s/%s_INS_data_ms.pdf' % (ss.outpath, obs))
