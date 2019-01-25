f7e896507968a9e768496ddf054004161a4606c7
Mike Wilensky <mjw768@uw.edu>
2019-01-24 16:17:46 -0800

    Actually write out uvfits files for diffuse survey

--- Diffuse/Diffuse_Original_Flag.py
+++ Diffuse/Diffuse_Original_Flag.py
@@ -10,13 +10,20 @@ parser.add_argument('outpath')
 parser.add_argument('--times', type=int, nargs='*', help='The time indices to flag')
 args = parser.parse_args()
 
-UV = UVData()
-UV.read(args.inpath, file_type='uvfits')
-JD = np.unique(UV.time_array)[-2]
-where = np.where(UV.time_array == JD)
-UV.flag_array[where] = 1
-auto_bl = np.where(UV.ant_1_array == UV.ant_2_array)
-UV.flag_array[auto_bl] = 1
+
+def UV_construct(args):
+    UV = UVData()
+    UV.read(args.inpath, file_type='uvfits')
+    JD = np.unique(UV.time_array)[-2]
+    where = np.where(UV.time_array == JD)
+    UV.flag_array[where] = 1
+    auto_bl = np.where(UV.ant_1_array == UV.ant_2_array)
+    UV.flag_array[auto_bl] = 1
+
+    return(UV)
+
+
+UV = UV_construct(args)
 
 shape_dict = {'TV6': [1.74e8, 1.81e8],
               'TV7': [1.81e8, 1.88e8],
@@ -27,8 +34,9 @@ shape_dict = {'TV6': [1.74e8, 1.81e8],
 
 sky_sub = SS(obs=args.obs, UV=UV, outpath=args.outpath, flag_choice='original')
 sky_sub.INS_prepare()
-sky_sub.INS.save()
-Catalog_Plot.INS_plot(sky_sub.INS)
 sky_sub.MF_prepare(sig_thresh=5, shape_dict=shape_dict, N_thresh=15)
 sky_sub.MF.apply_match_test(apply_N_thresh=True)
-Catalog_Plot.INS_plot(sky_sub.INS)
+
+sky_sub.apply_flags(choice='INS', INS=ss.INS)
+sky_sub.write('%s/%s.uvfits' % (args.outpath, args.obs), 'uvfits',
+              UV=UV_construct(args))
