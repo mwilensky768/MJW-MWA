1bc3dc8c910f64ad8edf0de2dc4d5bde8290beea
Mike Wilensky <mjw768@uw.edu>
2019-09-03 10:52:13 -0700

    add script for calculating DTV power

---
+++ HERA_RFI/DTV_stat.py
@@ -0,0 +1,49 @@
+import argparse
+import numpy as np
+from SSINS import INS, MF
+from SSINS.util import make_obslist
+from pyuvdata import UVData
+import yaml
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--ins_file', '-i', help='path to saved out SSINS')
+parser.add_argument('--yml_file', '-y', help='Path to match_events_file')
+parser.add_argument('--raw_file', '-r', help='Path to raw data file')
+parser.add_argument('--outfile', '-o', help='The file to write the stat to')
+args = parser.parse_args()
+
+ins = INS(args.ins_file, match_events_file=args.yml_file)
+
+uv = UVData()
+uv.read(args.raw_file)
+auto_uv = uv.select(ant_str='autos', inplace=False)
+uv.select(ant_str='crosses')
+
+shapes = ['TV4', 'TV5', 'TV6']
+shape_dict = {'TV4': [1.74e8, 1.82e8],
+              'TV5': [1.82e8, 1.9e8],
+              'TV6': [1.9e8, 1.98e8]}
+
+mf = MF(ins.freq_array, 5, shape_dict=shape_dict, streak=False, narrow=False)
+
+for event in ins.match_events:
+    if event[-2] in shapes:
+        ins.metric_array[event[:2]] = np.ma.masked
+
+ins_flags = ins.mask_to_flags()
+cross_vis_flags = np.repeat(ins_flags[:, np.newaxis, np.newaxis, :, :], uv.Nbls, axis=1)
+auto_vis_flags = np.repeat(ins_flags[:, np.newaxis, np.newaxis, :, :], auto_uv.Nbls, axis=1)
+uv.flag_array = cross_vis_flags.reshape([uv.Nblts, 1, uv.Nfreqs, uv.Npols])
+auto_uv.flag_array = auto_vis_flags.reshape([auto_uv.Nblts, 1, auto_uv.Nfreqs, auto_uv.Npols])
+
+stat_dict = {'TV4': {'occ': 0, 'autopow': 0, 'crosspow': 0},
+             'TV5': {'occ': 0, 'autopow': 0, 'crosspow': 0},
+             'TV6': {'occ': 0, 'autopow': 0, 'crosspow': 0}}
+
+for shape in shapes:
+    stat_dict[shape]['occ'] = np.count_nonzero(np.any(flags[:, mf.slice_dict[shape]], axis=(1, 2)))
+    stat_dict[shape]['autopow'] = np.mean(np.abs(auto_uv.data_array[auto_uv.flag_array[:, :, mf.slice_dict[shape]]]))
+    stat_dict[shape]['crosspow'] = np.mean(np.abs(uv.data_array[uv.flag_array[:, :, mf.slice_dict[shape]]]))
+
+with open(args.outfile, 'w') as file:
+    yaml.safe_dump(stat_dict, file, default_flow_style=False)
