4d0eda3293c28c49c8816dc30842f929831242e5
Mike Wilensky <mjw768@uw.edu>
2019-09-05 17:26:20 -0700

    DTV_occ_hist add

---
+++ HERA_RFI/DTV_occ_hist.py
@@ -0,0 +1,35 @@
+import numpy as np
+import yaml
+import argparse
+import matplotlib.pyplot as plt
+from SSINS.util import make_obslist
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--yml_file_list', help='The yml files to operate on')
+parser.add_argument('--obs_list', help="The list of obsids")
+parser.add_argument('--outdir', help="The output directory")
+args = parser.parse_args()
+
+obslist = make_obslist(args.obs_list)
+yml_list = make_obslist(args.yml_file_list)
+
+inner_keys = ['occ', 'autopow', 'crosspow']
+chan_keys = ['TV%i' % chan for chan in [4, 5, 6]]
+list_dict = {'%s' % chan_key for chan_key in chan_keys: {'%s': [] % key for key in inner_keys}}
+
+for yml, obs in zip(yml_list, obslist):
+    with open(yml, 'r') as yml_file:
+        obsdict = yaml.load(yml_file, Loader=yaml.Loader)
+    for chan in chan_keys:
+        for key in inner_keys:
+            list_dict[chan][key].append(obsdict[chan][key])
+
+Titles = ['Occupancy per Obs', 'Average Flagged Autocorrelation Per Obs', 'Average Flagged Cross-Correlation Per Obs']
+colors = ['organge', 'green', 'blue']
+
+for key, title in zip(inner_keys, Titles):
+    plt.hist([list_dict[chan][key] for chan in chan_keys], bins='auto', title=title,
+             label=chan_keys, bins='auto', histtype='step', color=colors)
+    for channel, color in zip(chan_keys, colors):
+        plt.axvline(x=np.mean(list_dict[channel][key]), color='color')
+    plt.savefig('%s/HERA_DTV_%s_hist.pdf' % (args.outdir, key))
