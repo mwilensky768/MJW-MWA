bfea813486d5525d5e127f80d7ca60085be07a97
Mike Wilensky <mjw768@uw.edu>
2019-10-22 07:56:23 +0800

    change output dir

--- Pawsey_Wrappers/Pawsey_submission_daemon.sh
+++ Pawsey_Wrappers/Pawsey_submission_daemon.sh
@@ -7,6 +7,6 @@ SSINS_dir="/group/mwaeor/mwilensky/2014_SSINS_outputs"
 script="/home/mwilensky/MJW-MWA/Pawsey_Wrappers/Pawsey_COTTER_SSINS_Full_Obs.sh"
 
 for pointing in minus_two minus_one zenith plus_one plus_two; do
-  bash $wrapper -f ${SSINS_dir}/2014_EoR_High_Pointing_${pointing}_obs.txt -o ${SSINS_dir}/2014_${pointing}_outputs -s $script -m 50G -t 02:00:00 -a 2 -b 80 -e 80 -i 2 -c 2
-  sleep 2h
+  bash $wrapper -f ${SSINS_dir}/2014_EoR_High_Pointing_${pointing}_obs.txt -o ${SSINS_dir}/2014_${pointing}_SSINS_outputs -s $script -m 50G -t 02:00:00 -a 2 -b 80 -e 80 -i 2 -c 2
+  sleep 3h
 done
