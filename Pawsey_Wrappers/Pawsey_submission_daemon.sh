2d9854f0b3cd5ef992bdd7e6718affccbed7d80a
Mike Wilensky <mjw768@uw.edu>
2019-10-21 15:39:12 +0800

    try some shit

--- Pawsey_Wrappers/Pawsey_submission_daemon.sh
+++ Pawsey_Wrappers/Pawsey_submission_daemon.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -l
 
 # Run this shit from tmux
 
@@ -7,6 +7,6 @@ wrapper="/home/mwilensky/MJW-MWA/Pawsey_Wrappers/Pawsey_COTTER_SSINS_Wrapper.sh"
 script="/home/mwilensky/MJW-MWA/Pawsey_Wrappers/Pawsey_COTTER_SSINS_Full_Obs.sh"
 
 for pointing in minus_two minus_one zenith plus_one plus_two; do
-  bash $wrapper -f ${SSINS_dir}/2014_EoR_High_Pointing_${pointing}_obs.txt -o ${SSINS_dir}/2014_${pointing}_outputs -s $script -m 50G -t 02:00:00 -a 2 -b 80 -e 80 -i 2 -c 2
+  bash $wrapper -f ${2014_SSINS_dir}/2014_EoR_High_Pointing_${pointing}_obs.txt -o ${2014_SSINS_dir}/2014_${pointing}_outputs -s $script -m 50G -t 02:00:00 -a 2 -b 80 -e 80 -i 2 -c 2
   sleep 2h
 done
